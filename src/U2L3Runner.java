402fa25cfee6ef298a61bb0ebb88afb5b3359ddb
BT_4N2_13 <yhuang6817@bths.edu>
2022-09-29 12:10:19 -0400

    tried out "game" class.

--- src/U2L3Runner.java
+++ src/U2L3Runner.java
@@ -1,16 +1,12 @@
 public class U2L3Runner {
     public static void main(String[] args) {
-        Box box = new Box(10.5, 12.6, 15.75);
+        Game game = new Game();
+        game.addPlayer(9);
+        game.increaseScore(10293);
+        System.out.println("Number of Players: " + game.getPlayers());
+        System.out.println("Score: " + game.getScore());
+        System.out.println("Avg score per player: " + game.averageScorePerPlayer());
 
-        // Use the return value from volume() by storing it in a variable
-        double boxVolume = box.volume();
-        System.out.println("My box has a volume of " + boxVolume);
-
-        // Use the return value from volume() by printing it
-        System.out.println("My box has a volume of " + box.volume());
-
-        // void methods are DESIGNED to be used as just a statement
-        box.printDimensions();  // this is how void methods should be used!
 
     }
 }
