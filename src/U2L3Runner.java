f548f2f8d61296e69ba54f7eec3c6577f48bb13d
BT_4N2_13 <yhuang6817@bths.edu>
2022-09-29 12:32:56 -0400

    tried out "game" class.

--- src/U2L3Runner.java
+++ src/U2L3Runner.java
@@ -1,11 +1,8 @@
 public class U2L3Runner {
     public static void main(String[] args) {
-        Game game = new Game();
-        game.addPlayer(9);
-        game.increaseScore(10293);
-        System.out.println("Number of Players: " + game.getPlayers());
-        System.out.println("Score: " + game.getScore());
-        System.out.println("Avg score per player: " + game.averageScorePerPlayer());
+        Letter letter = new Letter("Yun");
+        letter.writeLetter("Bob");
+
 
 
     }
